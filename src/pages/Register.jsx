d77ea794e2cea1c74b8ae35b55e315515ed2a1be
RosaBen <benchabane.rosa@outlook.com>
2025-09-15 12:33:19 +0200

    initial commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+export default function Register(){
+const [username, setUsername]= useState('')
+const [email, setEmail]=useState('')
+const [password, setPasword]=useState('')
+  return (
+    <>
+    </>
+  )
+}
